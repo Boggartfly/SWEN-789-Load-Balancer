ab0c2d63dd08c0480229807e8005c8d6d7990b10
Kirtana <ks3057@rit.edu>
2018-11-21 15:29:39 -0500

    added script folder & changed the scale up and down methods.

---
+++ src/Scripts/start_instance_8280.sh
@@ -0,0 +1,3 @@
+export CATALINA_BASE=/Users/kirtanasuresh/Documents/Tomcat/TomcatInstance8280
+cd /usr/local/apache-tomcat-9.0.12/bin/
+./startup.sh
---
+++ src/Scripts/start_instance_8580.sh
@@ -0,0 +1,3 @@
+export CATALINA_BASE=/Users/kirtanasuresh/Documents/Tomcat/TomcatInstance8580
+cd /usr/local/apache-tomcat-9.0.12/bin/
+./startup.sh
